b15a75325dcdcdd2c2ebb270e336f5337c644a95
Aesthuo <alx.lesperance@gmail.com>
2025-01-13 22:59:59 -0500

    Legless edition

--- unpaid_intern.gd
+++ unpaid_intern.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 @export var speed = 14
 @export var grav = 75
-var targ = Vector3.ZERO
+#var targ = Vector3.ZERO
 var dir = 0
 var rot = 0
 var endPos = Vector3.ZERO
@@ -10,34 +10,40 @@ var schmoove = false
 
 func _physics_process(delta):
 	
+	rot = 0
+
 	if not schmoove:
-		endPos = 0
 		dir = 0
 		if Input.is_action_just_pressed("MoveToTheForwards"):
 			dir = 1
 		if Input.is_action_just_pressed("MoveToTheBackwards"):
 			dir = -1
-		if Input.is_action_just_pressed("MoveToTheLeft"):
-			rot = -1
-		if Input.is_action_just_pressed("MoveToTheRight"):
-			rot = 1
+	#set values of dir to indicate transition direction
 	else:
 		pass
-		#here you make move
+
+	if Input.is_action_just_pressed("MoveToTheLeft"):
+		rot = -1
+	if Input.is_action_just_pressed("MoveToTheRight"):
+		rot = 1
+	#set values of rot to indicate rotation direction
 		
 	if dir != 0:
-		endPos.x = (sin($Pivot.rotation.y) * 5 * dir) + position.x
-		endPos.z = (cos($Pivot.rotation.y) * 5 * dir) + position.z
+		if not schmoove:
+			endPos.x = (sin($Pivot.rotation.y) * 5 * dir) + position.x
+			endPos.z = (cos($Pivot.rotation.y) * 5 * dir) + position.z
+			schmoove = true
 		#position = endPos
-		schmoove = true
+	#set value of end position and set shmoove to true to disable additional inputs
 
-	targ.x = sin($Pivot.rotation.y) * speed * dir
-	targ.z = cos($Pivot.rotation.y) * speed * dir
+	#targ.x = sin($Pivot.rotation.y) * speed * dir
+	#targ.z = cos($Pivot.rotation.y) * speed * dir
 		
-	$Pivot.rotate_y(rot*deg_to_rad(-45))
+	$Pivot.rotate_y(rot * deg_to_rad(-90))
 	if not is_on_floor():
-		targ.y = targ.y - (grav*delta)
-			
-	#velocity = targ
+		pass
+		#targ.y = targ.y - (grav*delta)
+	#functional rotation and non-functional gravity
+		
 	move_and_slide()
 
